7fdd9ebba1071036ab5ad5ad001eb6a70f0cc949
andrius111 <andrius.martini1994@gmail.com>
2020-04-21 13:45:32 -0300

    List taks improvements

--- src/components/ListTasks/ItensListTasks/ItensListTasks.jsx
+++ src/components/ListTasks/ItensListTasks/ItensListTasks.jsx
@@ -12,49 +12,68 @@ const ItensListTasks = (props) => {
     return task.completed ? 'line-through' : 'none'
   }
   
-  return (
-    props.tasks.map(task => (
-      <tr 
-        key={ task.id } 
-        data-testid="tr-task"
-      >
+  function getEmptyTasks() {
+    return (
+      <tr key="tr-empty">
         <td
-          className="text-left"
-          data-testid="td-task-name"
-          style={{ textDecoration: setCompleted(task) }}
+          className="text-center"
+          colSpan="2"
         >
-          { task.name }
-        </td>  
-
-        <td 
-          className="text-right"
-          width="140px"
-        >
-          <FinishTask 
-            task={ task }
-            reloadTasks={ props.reloadTasks }
-            className={ task.completed ? 'hidden' : '' }
-          />
-          
-          <Link 
-            to={ "/update/" + task.id }
-            className={ task.completed ? 'hidden' : 'btn btn-warning btn-sm' }
-            style={{ 
-              marginLeft: '10px',
-              marginRight: '10px'
-            }}
-            title="Edit the task"
-          >
-            <FontAwesomeIcon icon={ faEdit } />
-          </Link>
-
-          <RemoveTask 
-            task={ task }
-            reloadTasks={ props.reloadTasks }
-          />
-        </td>
+          <i>No tasks have been registered yet</i> 
+        </td> 
       </tr>
-    ))
+    )
+  }
+
+  function getItensList() {
+    return (
+      props.tasks.map(task => (
+        <tr 
+          key={ task.id } 
+          data-testid="tr-task"
+        >
+          <td
+            className="text-left"
+            data-testid="td-task-name"
+            style={{ textDecoration: setCompleted(task) }}
+          >
+            { task.name }
+          </td>  
+
+          <td 
+            className="text-right"
+            width="140px"
+          >
+            <FinishTask 
+              task={ task }
+              reloadTasks={ props.reloadTasks }
+              className={ task.completed ? 'hidden' : '' }
+            />
+            
+            <Link 
+              to={ "/update/" + task.id }
+              className={ task.completed ? 'hidden' : 'btn btn-warning btn-sm' }
+              style={{ 
+                marginLeft: '10px',
+                marginRight: '10px'
+              }}
+              title="Edit the task"
+            >
+              <FontAwesomeIcon icon={ faEdit } />
+            </Link>
+
+            <RemoveTask 
+              task={ task }
+              reloadTasks={ props.reloadTasks }
+            />
+          </td>
+        </tr>
+      ))
+    )
+  } 
+
+  return (
+    props.tasks.length ? getItensList() : getEmptyTasks()
   )
 }
 
